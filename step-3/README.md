9832547528667731cf250ffc5026606fa6d1fc4f
xuxiaokun <996670910@qq.com>
2017-02-03 18:12:13 +0800

    add step-3

---
+++ step-3/README.md
@@ -0,0 +1,10 @@
+# 安装 Vue.js
+```
+npm install vue
+```
+# 如何运行 Vue
+1. 将文件拷贝到本地
+2. 安装安装 Node.js 和 NPM
+3. ``npm i``
+4. 运行 ``webpack`` 
+5. 打开 page.html
---
+++ step-3/app.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    newTodo: '',
+    todoList: []
+  },
+  craeted: function(){
+  	window.onbeforeunload = ()=>{
+  		let dataString = JSON.stringify(this.tosoList)
+  		window.localStrorage.setItem('myTodos', dataString)
+  	}
+
+  	let oldDataString = window.localStrorage.getItem('myTodos')
+  	let oldData = JSON.parse(oldDataString)
+  	this.todoList = oldData || []
+  	
+  },
+  methods: {
+    addTodo: function(){
+      this.todoList.push({
+        title: this.newTodo,
+        createdAt: new Date(),
+        done: false // 添加一个 done 属性
+      })
+      this.newTodo = '' // 变成空
+    },
+    removeTodo: function(todo){
+    	let index = this.todoList.indexOf(todo)
+    	this.todoList.splice(index,1)
+    }
+  }
+})      
+
