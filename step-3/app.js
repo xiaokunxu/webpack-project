121521a1fa34a2f8dae40a46128cd48d965007c4
xuxiaokun <996670910@qq.com>
2017-02-05 14:56:46 +0800

    修改

--- step-3/app.js
+++ step-3/app.js
@@ -1,35 +1,54 @@
 import Vue from 'vue'
 
 var app = new Vue({
-  el: '#app',
-  data: {
-    newTodo: '',
-    todoList: []
-  },
-  craeted: function(){
-  	window.onbeforeunload = ()=>{
-  		let dataString = JSON.stringify(this.tosoList)
-  		window.localStrorage.setItem('myTodos', dataString)
-  	}
-
-  	let oldDataString = window.localStrorage.getItem('myTodos')
-  	let oldData = JSON.parse(oldDataString)
-  	this.todoList = oldData || []
-
-  },
-  methods: {
-    addTodo: function(){
-      this.todoList.push({
-        title: this.newTodo,
-        createTime: new Date().Format("yyyy-MM-dd hh:mm:ss"),
-        done: false // 添加一个 done 属性
-      })
-      this.newTodo = '' // 变成空
+	    el: '#app',
+	    data: {
+	    newTodo: '',
+	    todoList: []
     },
+  
+    methods: {
+	    addTodo: function(){
+	    	var date = new Date();
+	    	var year = date.getFullyeal(),
+	    		month = parseInt(date.getMonth()+1),
+	    		day = date.getDate(),
+	    		hours = date.getHours(),
+	    		min = date.getMinutes(),
+	    		sec = date.getSeconds();
+
+	    	var time = year + "-"
+	    		+ (month<10?"0":"")+month+"-"
+	    		+ (day<10?"0":"")+day+"-"
+	    		+ (hours<10?"0":"")+hours+":"
+	    		+ (min<10?"0":"")+min+":"
+	    		+ (sec<10?"0":"")+sec+;
+	        this.todoList.push({
+	            title: this.newTodo,
+	        	createTime: time,
+	        	done: false // 添加一个 done 属性
+	        })
+	    this.newTodo = '' // 变成空
+    }, 
     removeTodo: function(todo){
-    	let index = this.todoList.indexOf(todo)
-    	this.todoList.splice(index,1)
+    		let index = this.todoList.indexOf(todo)
+    		this.todoList.splice(index,1)
+    	}
+    },
+    craeted: function(){
+  		window.onbeforeunload = ()=>{
+  			let dataString = JSON.stringify(this.tosoList)
+  			let newTodoString = JSON.stringify(this.newTodo)
+  			window.localStrorage.setItem('myTodos', dataString)
+  		}
+
+  		let oldDataString = window.localStrorage.getItem('myTodos')
+  		let newTodoString = window.localStrorage.getItem('newTodoString')
+
+  		let oldData = JSON.parse(oldDataString)
+  		let newTodo = JSON.parse(newTodoString)
+  		this.todoList = oldData || []
+  		this.newTodo = newTodo || ""
     }
-  }
 })      
 
