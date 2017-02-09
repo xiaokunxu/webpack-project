0222f8f11aa2a061aa7b82fc8d62c3c8ee6a38d8
xuxiaokun <996670910@qq.com>
2017-02-09 23:19:43 +0800

    init

---
+++ resumer/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>{{text}}</p>
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      text: '你好',
+      msg: 'hello'
+    }
+  }
+}
+</script>
+
+<style>
+p {
+  color: red
+}
+</style>
