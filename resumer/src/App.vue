4f8f51754b811d9cf90e823e7fb718d9d47b6e08
xuxiaokun <996670910@qq.com>
2017-02-10 14:17:37 +0800

    新建ResumeEditor.vue、ResumePreview.vue、Topbar.vue三个文件，修改样式

--- resumer/src/App.vue
+++ resumer/src/App.vue
@@ -1,24 +1,61 @@
 <template>
-  <div>
-    <p>{{text}}</p>
-    <p>{{msg}}</p>
+  <div class="page"> 
+    <header>
+      <Topbar/>
+    </header>
+    <main>  
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
   </div>
 </template>
 
 <script>
+import 'normalize.css/normalize.css'
+import './assets/reset.css'
+import Topbar from './components/Topbar'
+import ResumeEditor from './components/ResumeEditor'
+import ResumePreview from './components/ResumePreview'
+
 export default {
   name: 'app',
   data: function () {
-    return {
+    return { 
       text: '你好',
-      msg: 'hello'
     }
-  }
+  },
+  components: {Topbar, ResumeEditor, ResumePreview}
 }
 </script>
 
 <style>
-p {
-  color: red
-}
+  .page{
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background: #EAEBEC;
+  }
+  .page > main{
+    flex-grow: 1;
+  }
+  .page > main{
+    min-width: 1024px;
+    max-width: 1440px;
+    margin-top: 16px;
+    margin-bottom: 16px;
+    display: flex;
+    justify-content: space-between;
+    padding: 0 16px;
+    width: 100%;
+    align-self: center;
+  }
+  #resumeEditor{
+    width: 35%;
+    background: #444;
+  }
+  #resumePreview{
+  flex-grow: 1;
+  margin-left: 16px;
+  background: #777;
+  }
 </style>
---
+++ resumer/src/components/ResumePreview.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="resumePreview">
+    I am resumePreview
+</template>
+
+<script>
+export default {
+  name: 'ResumePreview'
+}
+</script>
+
+<style>
+#resumeEditor{
+	background: #fff;
+  	box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .25);
+}
+</style>
