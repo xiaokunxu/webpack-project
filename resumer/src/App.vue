ce5bb34e592cfa7f628cb009bc7e28feaf2c599e
xuxiaokun <996670910@qq.com>
2017-02-21 19:50:17 +0800

    将 SVG 插入 body 中

--- resumer/src/App.vue
+++ resumer/src/App.vue
@@ -16,6 +16,7 @@ import './assets/reset.css'
 import Topbar from './components/Topbar'
 import ResumeEditor from './components/ResumeEditor'
 import ResumePreview from './components/ResumePreview'
+import icons from './assets/icon'
 
 export default {
   name: 'app',
@@ -24,38 +25,41 @@ export default {
       text: '你好',
     }
   },
-  components: {Topbar, ResumeEditor, ResumePreview}
+  components: {Topbar, ResumeEditor, ResumePreview},
+  created(){
+    document.body.insertAdjacentHTML('afterbegin', icon)
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
   .page{
     height: 100vh;
     display: flex;
     flex-direction: column;
     background: #EAEBEC;
-  }
-  .page > main{
-    flex-grow: 1;
-  }
-  .page > main{
-    min-width: 1024px;
-    max-width: 1440px;
-    margin-top: 16px;
-    margin-bottom: 16px;
-    display: flex;
-    justify-content: space-between;
-    padding: 0 16px;
-    width: 100%;
-    align-self: center;
+    > main{
+      flex-grow: 1;
+    }
+    > main{
+      min-width: 1024px;
+      max-width: 1440px;
+      margin-top: 16px;
+      margin-bottom: 16px;
+      display: flex;
+      justify-content: space-between;
+      padding: 0 16px;
+      width: 100%;
+      align-self: center;
+    }
   }
   #resumeEditor{
     width: 35%;
     background: #444;
   }
   #resumePreview{
-  flex-grow: 1;
-  margin-left: 16px;
-  background: #777;
+    flex-grow: 1;
+    margin-left: 16px;
+    background: #777;
   }
 </style>
