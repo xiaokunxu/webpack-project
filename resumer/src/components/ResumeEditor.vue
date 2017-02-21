4c6baf66115333414b5eeb48679423f8ba396e01
xuxiaokun <996670910@qq.com>
2017-02-21 19:09:16 +0800

    添加 Tab 切换

--- resumer/src/components/ResumeEditor.vue
+++ resumer/src/components/ResumeEditor.vue
@@ -1,17 +1,92 @@
 <template>
-  <div id="resumeEditor">
-    I am resumeEditor
+  	<div id="resumeEditor">
+	    <nav>
+	    	<ol>
+	    		<li v-for="(item, index) in resume.visibleItems"
+					:class="{active: item === selected}"
+					@click="selected = item"
+	    		>
+	    		{{index}}
+	    		</li>
+	    	</ol>
+	    </nav>
+	    <ol class="panels">
+	    	<li v-for="item in resume.visibleItems" v-show="item === selected">
+	    		{{resume[item]}}
+	    	</li>
+	    </ol>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'ResumeEditor'
-}
+	export default {
+	  	name: 'ResumeEditor',
+	  	data() {
+	  		return {
+	  			selected: 'bio',
+	  			resume: {
+	  				visibleItems: ['bio', 'work history', 'education', 'projects', 'awards', 'contacts', 'others'],
+	  				bio: {
+	  					name: '',
+	  					city: '',
+	  					title: ''
+	  				},
+	  				'work history': [],
+	  				education: [],
+	  				projects: [],
+	  				awards: [],
+	  				contacts: [],
+	  				others: []
+	  			}
+	  		}
+	  	}
+	}
 </script>
 
-<style>
+<style lang="scss">
 #resumeEditor{
-  background: #fff;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .25);
+  	background: #fff;
+  	box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .25);
+	display: flex;
+	flex-direction: row;
+	overflow: auto;
+	> nav{
+		width: 80px;
+		background: #000;
+		color: #fff;
+		> ol{
+			> li{
+				height: 48px;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				margin-top: 16px;
+				margin-bottom: 16px;
+				&.active{
+					background: #fff;
+					color: #000;
+				}
+			}
+		}
+	}
+}
+ol{
+	list-style: none;
 }
 </style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
