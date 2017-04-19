00cf32e34bb0bd889cfc8e7b1ae0a4143655cd3e
xuxiaokun <996670910@qq.com>
2017-04-19 15:34:13 +0800

    将原有数据移动到 store 里

--- resumer/src/components/ResumeEditor.vue
+++ resumer/src/components/ResumeEditor.vue
@@ -29,6 +29,10 @@
 				<input type="text" v-model="resume[item.field][key]">
 		    </div>
 	    	</li>
+	    	<li>
+	    		{{count}}
+	    		<button @click="add">test</button>
+	    	</li>
 	    </ol>
 	</div>
 </template>
@@ -36,46 +40,62 @@
 <script>
 	export default {
 	  	name: 'ResumeEditor',
-	  	data() {
-	  		return {
-	  			selected: 'profile',
-	  			resume: {
-	  				// visibleItems: ['profile', 'work history', 'education', 'projects', 'awards', 'contacts', 'others'],
-	  				config: [
-	  					{ field: 'profile', icon: 'id' },
-	          			{ field: 'work history', icon: 'work' },
-	         			{ field: 'education', icon: 'book' },
-	         			{ field: 'projects', icon: 'heart' },
-	          			{ field: 'awards', icon: 'cup' },
-				        { field: 'contacts', icon: 'phone' },
-	  				],
-	  				profile: {
-	  					name: '徐XX',
-	  					city: '火星',
-	  					title: '前端工程师'
-	  				},
-	  				'work history': [
-	  					{ company: 'AL', content: '我的第二份工作是' },
-	  					{ company: 'TX', content: '我的第一份工作是' },
-	  				],
-	  				education: [
-	  					{ school: '社会大学', content: '研究生' },
-	  					{ school: '社会大学', content: '本科' },
-	  				],
-	  				projects: [
-	  					{ name: '项目2', content: '文字' },
-	  					{ name: '项目1', content: '文字' },
-	  				],
-	  				awards: [
-	  					{ name: 'awards A', content: '文字' },
-	  					{ name: 'awards B', content: '文字' },
-	  				],
-	  				contacts: [
-	  					{ contact: '手机', content: '13412434567' },
-	  					{ contact: 'QQ', content: '12345678' },
-	  				],
-	  				// others: []
-	  			}
+	  	// data() {
+	  	// 	return {
+	  	// 		selected: 'profile',
+	  	// 		resume: {
+	  	// 			// visibleItems: ['profile', 'work history', 'education', 'projects', 'awards', 'contacts', 'others'],
+	  	// 			config: [
+	  	// 				{ field: 'profile', icon: 'id' },
+	   //        			{ field: 'work history', icon: 'work' },
+	   //       			{ field: 'education', icon: 'book' },
+	   //       			{ field: 'projects', icon: 'heart' },
+	   //        			{ field: 'awards', icon: 'cup' },
+				//         { field: 'contacts', icon: 'phone' },
+	  	// 			],
+	  	// 			profile: {
+	  	// 				name: '徐XX',
+	  	// 				city: '火星',
+	  	// 				title: '前端工程师'
+	  	// 			},
+	  	// 			'work history': [
+	  	// 				{ company: 'AL', content: '我的第二份工作是' },
+	  	// 				{ company: 'TX', content: '我的第一份工作是' },
+	  	// 			],
+	  	// 			education: [
+	  	// 				{ school: '社会大学', content: '研究生' },
+	  	// 				{ school: '社会大学', content: '本科' },
+	  	// 			],
+	  	// 			projects: [
+	  	// 				{ name: '项目2', content: '文字' },
+	  	// 				{ name: '项目1', content: '文字' },
+	  	// 			],
+	  	// 			awards: [
+	  	// 				{ name: 'awards A', content: '文字' },
+	  	// 				{ name: 'awards B', content: '文字' },
+	  	// 			],
+	  	// 			contacts: [
+	  	// 				{ contact: '手机', content: '13412434567' },
+	  	// 				{ contact: 'QQ', content: '12345678' },
+	  	// 			],
+	  	// 			// others: []
+	  	// 		}
+	  	// 	}
+	  	// },
+	  	computed: {   // 只用于读取数据
+	  		count () {
+	  			return this.$store.state.count
+	  		},
+	  		selected () {
+	  			return this.$store.state.selected
+		  	},
+		  	resume () {
+		  		return this.$store.state.resume
+		  	}
+	  	},
+	  	methods: {
+	  		add () {
+	  			this.$store.commit('increment')
 	  		}
 	  	}
 	}
