be5c6619f42793d544517fbf514e871d5968fecc
xuxiaokun <996670910@qq.com>
2017-04-20 17:54:46 +0800

    show resume

--- resumer/src/components/ResumePreview.vue
+++ resumer/src/components/ResumePreview.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="resumePreview">
-    I am resumePreview
+    {{resume}}
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'ResumePreview'
-}
+    export default {
+        name: 'ResumePreview',
+        computed: {
+            resume() {
+                return this.$store.state.resume
+            }
+        }
+    }
 </script>
 
 <style>
-#resumePreview{
-	background: #fff;
-  	box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .25);
-}
+    #resumePreview {
+        background: #fff;
+        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .25);
+    }
 </style>
