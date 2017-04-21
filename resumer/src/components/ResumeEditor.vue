0ae3b4e37c2367021e7794c061164ca9bb35e153
xuxiaokun <996670910@qq.com>
2017-04-21 17:07:13 +0800

    使用 Vuex 思想重构

--- resumer/src/components/ResumeEditor.vue
+++ resumer/src/components/ResumeEditor.vue
@@ -26,7 +26,7 @@
         	</div>
         	<div v-else class="resumeField" v-for="(value,key) in resume[item.field]">
 		    	<label> {{key}} </label>
-				<input type="text" v-model="resume[item.field][key]">
+				<input type="text" :value="value" @input="changeResumeField(item.field, key, $event.target.value)">
 		    </div>
 	    	</li>
 	    </ol>
@@ -49,7 +49,15 @@
                 return this.$store.state.resume
             }
         },
-        methods: {}
+        methods: {
+            changeResumeField(field, subfield, value) {
+                this.$store.commit('updateResume', {
+                    field,
+                    subfield,
+                    value
+                })
+            }
+        }
     }
 </script>
 
--- resumer/src/store/index.js
+++ resumer/src/store/index.js
@@ -47,6 +47,9 @@ export default new Vuex.Store({
     mutations: {
         switchTab(state, payload) {
             state.selected = payload
+        },
+        updateResume(state, { field, subfield, value }) {
+            state.resume[field][subfield] = value
         }
     }
 })
